b428befb26b30fb8e3cb98bc001f42a6891b2f48
adamwreed93 <68203005+adamwreed93@users.noreply.github.com>
2021-06-13 20:18:48 -0700

    Finished Game (Will be doing one more commit with the game build included!)

---
+++ The Great Fleece/Assets/The Great Fleece/Game/Scripts/Menu/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] private Image _progressBar;
+
+    void Start()
+    {
+        StartCoroutine(LoadLevelAsync());
+    }
+
+    IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+        
+        while (operation.isDone == false)
+        {
+            _progressBar.fillAmount = operation.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
